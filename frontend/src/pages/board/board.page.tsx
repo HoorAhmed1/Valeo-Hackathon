63f76acf47cedef233a7069f257530da979e687d
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-07-17 13:29:42 +0300

    refactor: Update the board page

--- frontend/src/pages/board/board.page.tsx
+++ frontend/src/pages/board/board.page.tsx
@@ -1,18 +1,24 @@
 import Button from "../../components/button/button.component";
-import { DataContainer, Holder, NavBar, PageContainer, PersonProfile, SearchBar, } from "./board.styles";
+import { DataContainer, Holder, ImagesHolder, NavBar, PageContainer, PersonProfile, SearchBar, TasksBox, TasksTitleHolder, TaskTitleHolder, } from "./board.styles";
 import personalImage from "../../assets/user.jpg"
 import { PageTitle } from "../../index.styles";
-import { IoMdSearch } from "react-icons/io";
+import { IoMdSearch,IoMdNotifications } from "react-icons/io";
+import Accordion from "../../components/accordion/accordion.component";
+import { TbGraphFilled } from "react-icons/tb";
+
 const BoardPage = () => {
     return (<PageContainer>
         <NavBar>
             <Button>
                 Create
             </Button>
-            <PersonProfile src={personalImage} alt="personal image" />
+            <div className="flex gap-4 items-center">
+                <IoMdNotifications className="cursor-pointer" size={28} color='var(--slate-400)' title='notification' />
+                <PersonProfile src={personalImage} alt="personal image" />
+            </div>
         </NavBar>
         <Holder>
-            <div className="flex flex-col justify-between gap-4">
+            <div className="flex flex-col justify-between gap-4 py-4 h-full">
             <PageTitle className={'text-left'} >
               BOARD
              </PageTitle>
@@ -24,10 +30,47 @@ const BoardPage = () => {
                         />
                </SearchBar>     
             </div>
-            
+            <div className="flex-1 flex justify-between pt-10 pl-4">
+                <ImagesHolder>
+                    <PersonProfile title="person name" src={personalImage} index="10"/>
+                    <PersonProfile src={personalImage} index="9"/>
+                    <PersonProfile src={personalImage} index="8"/>
+                    <PersonProfile src={personalImage} index="7"/>
+                    <PersonProfile src={personalImage} index="6"/>
+                </ImagesHolder>
+                <Button>
+                    <span className="flex gap-2">
+                    <TbGraphFilled size={20}/>
+                    Insight
+                    </span>
+                </Button>
+            </div>
         </Holder>
         <DataContainer>
-
+        <TasksTitleHolder>
+            <TaskTitleHolder>
+                <h1>BLOCKAGE</h1>
+                <p>12</p>
+            </TaskTitleHolder>
+            <TaskTitleHolder>
+                <h1>TODO</h1>
+                <p>1</p>
+            </TaskTitleHolder>
+            <TaskTitleHolder>
+                <h1>IN REVIEW</h1>
+                <p>20</p>
+            </TaskTitleHolder>
+            <TaskTitleHolder>
+                <h1>DONE</h1>
+                <p>12</p>
+            </TaskTitleHolder>
+        </TasksTitleHolder>
+        <Accordion title="Project 1" >
+<TasksBox></TasksBox>
+<TasksBox></TasksBox>
+<TasksBox></TasksBox>
+<TasksBox></TasksBox>
+        </Accordion>
         </DataContainer>
     </PageContainer>)
 }
--- frontend/src/pages/board/board.styles.ts
+++ frontend/src/pages/board/board.styles.ts
@@ -4,11 +4,15 @@ import styled, { css } from 'styled-components';
 export const PageContainer = styled(motion.div)`
     display: flex;
     flex-direction: column;
-    height: 100vh;
+    min-height: 100vh;
     padding: 0 0 0 30px;
-    background-color: #F3F4F8;
+    background-color: var(--gray-300);
     margin: 0;
     gap: 25px;
+    @media (max-width: 1024px) {
+    min-width: 1025px;
+     overflow-x: auto;
+    }
 `;
 const commonInputStyles = css`
   width: 100%;
@@ -23,14 +27,20 @@ ${commonInputStyles}
 align-items: center;
 flex-direction: row;
 justify-content: space-between; 
-height: 70px;
+padding-top: 10px;
+padding-bottom: 10px;
 border-radius: 0px 0px 0px 18px;
 `
-export const PersonProfile =styled.img`
+export const PersonProfile =styled.img<{ index?: string }>`
     width: 40px;
     height: 40px;
+    z-index: ${({ index }) =>  index ? index : '10'};
     border-radius: 50%;
     aspect-ratio: 1/1;
+    border: 2px solid #FCFBFB;
+  margin-right:  ${({ index }) =>  index ? '-10px' : ''};
+  cursor: pointer;
+
 `
 
 export const Holder =styled.div`
@@ -43,8 +53,12 @@ border-radius: 18px 0px 0px 18px;
 export const DataContainer =styled.div`
 flex:1;
 ${commonInputStyles}
-border-radius: 18px 0px 0px 0px;
+    flex-direction:column;
+    border-radius: 18px 0px 0px 0px;
+    padding: 2rem;
+    align-items:flex-start;
 `
+
 export const SearchBar = styled.div`
     display: flex;
     align-items: center;
@@ -65,3 +79,36 @@ export const SearchBar = styled.div`
         flex: 1;
     }
 `;
+
+export const ImagesHolder = styled.div`
+display:flex;
+align-items: center;
+position: relative; 
+`
+export const TasksTitleHolder =styled.div`
+    display: grid;
+    gap: 2rem;
+    width:100%;
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+`
+export const TaskTitleHolder =styled.div`
+border-radius:9px;
+display:flex;
+align-items: center;
+justify-content: space-between; 
+color: var(--slate-400);
+padding:0.65rem;
+background-color: var(--gray-300);
+h1{
+    font-weight: 700;
+}
+`
+export const TasksBox =styled.div`
+  border-radius:9px;
+display:flex;
+align-items: center;
+justify-content:center;
+padding:0.35rem;
+background-color: var(--gray-300);
+min-height:70vh;
+`
